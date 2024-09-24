d3b6bbb0a62e5373190e263673f47fdca4fadb96
pollocandente <a.23308051281211@cbtis128.edu.mx>
2024-09-23 20:48:50 -0600

    Add files via upload

---
+++ promedio.py
@@ -0,0 +1,11 @@
+print("")#define una linea en blanco
+print("Roman De la Cruz Leonardo Antonio. promedio")
+print(" ")#define una linea en blanco
+#solicita los valores
+numero1 = float(input("Introduce la calificacion: "))
+numero2 = float(input("Introduce la calificacion: "))
+numero3 = float(input("Introduce la calificacion "))
+
+suma=(numero1+numero2+numero3) / 3#operacion usada
+print(" ")#define una linea en blanco
+print("el promedio es",suma)#print imprime resultado
